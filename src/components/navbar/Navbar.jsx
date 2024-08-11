0871d141e04415b6e244dd37ac5decdf0e3d496c
Master God <gurrakhakhatri@gmail.com>
2024-08-11 12:52:58 +0530

    testing image issue

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         </motion.span>
         <div className="social">
           <a href="#">
-            <img src="/facebook.png" alt="fb" />
+            <img src="/portfolio-assignment/facebook.png" alt="fb" />
           </a>
           <a href="#">
             <img src="/instagram.png" alt="ig" />
